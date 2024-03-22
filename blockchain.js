89e84733a524127868048820702c1e2ac0ac6642
pacanosiu <60273697+DamianPacanowski@users.noreply.github.com>
2024-03-22 10:34:08 +0100

    Update blockchain.js

--- blockchain.js
+++ blockchain.js
@@ -1,18 +1,5 @@
 function blockchain() 
 {
-	var email_entry = document.getElementById("email_from_terms_and_conditions_of_connection").value;	
-	if(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(email_entry) !== false)
-	{
-		var entry = email_entry;
-		var redirect = "email="
-	}
-	else
-	{
-		document.getElementById("email_from_terms_and_conditions_of_connection").style.border= "1px solid rgb(255,0,0)";
-		document.getElementById("email_from_terms_and_conditions_of_connection").style.color= "rgb(255,0,0)";
-		document.getElementById("error").style.display= "block";
-	}
-	
 	if(typeof entry !== "undefined")
 	{
 		const net = "1234567890ABCDEFGHIJKLMNOPRSTUWXYVQZabcdefghijklmnoprstuwxyvqz@.=_-+*/!#%|";					
