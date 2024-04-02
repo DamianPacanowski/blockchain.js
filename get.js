8b3084cd36d854967693df18f460927b4b4e4911
sensor <60273697+DamianPacanowski@users.noreply.github.com>
2024-04-02 17:06:09 +0200

    Create get.js

---
+++ get.js
@@ -0,0 +1,18 @@
+if(document.getElementById("net"))
+{
+	var net = document.getElementById("net").value;
+}
+else
+{
+	var net = "1234567890ABCDEFGHIJKLMNOPRSTUWXYVQZabcdefghijklmnoprstuwxyvqz@.=_-+*/!#%|";
+}
+if(document.getElementById("text"))
+{
+	var text = document.getElementById("text").value;
+}
+else
+{
+	var text = "";
+}		
+var entry = text;
+var redirect = "?"
